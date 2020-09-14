3aef8c9af609db34ed8ddc1cc51c792c2b15e86a
sourav2831 <sourav2831@gmail.com>
2020-09-15 04:21:02 +0530

    Updates

---
+++ precache-manifest.de64f8562a4ffe86154a2c920ca232a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ea9746a3726dcaf6754819c2a005d61",
+    "url": "/tiger-survey/index.html"
+  },
+  {
+    "revision": "a56bca52cbacb527b5d8",
+    "url": "/tiger-survey/static/css/main.3ddbd657.chunk.css"
+  },
+  {
+    "revision": "21fabec567897b7c354b",
+    "url": "/tiger-survey/static/js/2.c166ca81.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/tiger-survey/static/js/2.c166ca81.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a56bca52cbacb527b5d8",
+    "url": "/tiger-survey/static/js/main.4fc11143.chunk.js"
+  },
+  {
+    "revision": "3c010a26a13b59f0072a",
+    "url": "/tiger-survey/static/js/runtime-main.2b850a70.js"
+  }
+]);
